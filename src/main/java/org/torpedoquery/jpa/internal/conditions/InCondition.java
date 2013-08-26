48016b286ab2615732c026641b1c79c0c1f28028
Xavier Jodoin <xavier.jodoin@corp.ovh.com>
2013-08-26 09:42:41 -0400

    update license header

--- src/main/java/org/torpedoquery/jpa/internal/conditions/InCondition.java
+++ src/main/java/org/torpedoquery/jpa/internal/conditions/InCondition.java
@@ -1,6 +1,5 @@
 /**
- *
- *   Copyright 2011 Xavier Jodoin xjodoin@gmail.com
+ *   Copyright Xavier Jodoin xjodoin@torpedoquery.org
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
